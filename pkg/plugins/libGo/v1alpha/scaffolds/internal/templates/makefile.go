7e23ae2d2761abbc3879dea8a34e0126c1b912c6
varshaprasad96 <varshaprasad96@gmail.com>
2022-01-11 14:08:04 -0800

    fix the path of main.go while building the project

--- pkg/plugins/libGo/v1alpha/scaffolds/internal/templates/makefile.go
+++ pkg/plugins/libGo/v1alpha/scaffolds/internal/templates/makefile.go
@@ -111,10 +111,10 @@ test: manifests generate fmt vet envtest ## Run tests.
 ##@ Build
 
 build: generate fmt vet ## Build manager binary.
-	go build -o bin/manager main.go
+	go build -o bin/manager cmd/main.go
 
 run: manifests generate fmt vet ## Run a controller from your host.
-	go run ./main.go
+	go run ./cmd/main.go
 
 docker-build: test ## Build docker image with the manager.
 	docker build -t ${IMG} .
